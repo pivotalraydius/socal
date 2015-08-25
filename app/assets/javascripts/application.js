04674eb8eef9a92d5041e3ef51d446fc378c9779
thin <thin@herenow.io>
2015-08-25 17:27:47 +0800

    rake assets precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-
 //= require jquery_ujs
 //= require turbolinks
 //= require react
@@ -23,7 +22,3 @@
 //= require horizontal-scroll
 
 //= require_tree .
-
-$(function(){
-    $("body").addClass("blur-bgimage");
-})
