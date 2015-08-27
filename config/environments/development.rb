95212df86ff0060d52efab49639eddcdd14c2892
thin <thin@herenow.io>
2015-08-27 17:41:46 +0800

    add react variant to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  #config.react.variant = :development
+  config.react.variant = :development
 
   config.web_console.whitelisted_ips = '192.168.0.108'
 
